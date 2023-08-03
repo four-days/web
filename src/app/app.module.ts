f5c2a585f0d0e78069e1fe0b14cdaf221975abcb
김경석 <0xe82de@gmail.com>
2023-08-04 00:04:24 +0900

    #42 config: 라우팅 기능 추가

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,38 @@
-import {APP_INITIALIZER, NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppComponent} from './app.component';
-import {ShortenUrlComponent} from './url/shorten-url.component';
-import {FormsModule} from "@angular/forms";
-import {HttpClientModule} from "@angular/common/http";
-import {AppConfigService} from "./config/app-config.service";
+import { AppComponent } from './app.component';
+import { ShortenUrlComponent } from './url/shorten-url.component';
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { AppConfigService } from "./config/app-config.service";
+import { RouterModule, Routes } from "@angular/router";
+import { RedirectComponent } from './redirect/redirect.component';
+
+const router: Routes = [
+  {
+    path: '',
+    component: ShortenUrlComponent,
+  },
+  {
+    path: '**',
+    component: RedirectComponent,
+  }
+  // {path: '', redirectTo: '/home', pathMatch: 'full'},
+  // {path: 'home', component: ShortenUrlComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    ShortenUrlComponent
+    ShortenUrlComponent,
+    RedirectComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(router, {enableTracing: false}),
   ],
   providers: [
     {
